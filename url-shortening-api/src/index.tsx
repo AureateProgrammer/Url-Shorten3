3de4ca5fe64d6326e1820d335e5f7103d667601c
AureteProgrammer <CodingBlakeMorris@gmail.com>
2026-04-12 18:42:51 -0400

    starting on react app

---
+++ url-shortening-api/src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './styles.css' // This is the professional CSS you just finished!
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
