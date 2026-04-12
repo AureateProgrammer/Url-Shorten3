b8aa43408675a0cf03d3904b7cfbf31cc666717e
AureteProgrammer <CodingBlakeMorris@gmail.com>
2026-04-12 18:57:49 -0400

    starting on react app

--- url-shortening-api/src/index.tsx
+++ url-shortening-api/src/index.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './styles.css' // This is the professional CSS you just finished!
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './styles.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById('root');
+
+if (!rootElement) {
+  throw new Error("Failed to find the root element. Check your index.html.");
+}
+
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
